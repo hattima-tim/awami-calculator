ad0b087c233fa3aeaa1a34f4da4bb49336183238
hattima-tim <irfanridwan20@gmail.com>
2025-05-11 21:55:26 +0600

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 // import { ThemeProvider } from "@/components/theme-provider"
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -36,6 +37,7 @@ export default function RootLayout({
         {/* <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange> */}
         {children}
         {/* </ThemeProvider> */}
+        <Analytics />
       </body>
     </html>
   );
