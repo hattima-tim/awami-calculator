feac5a4792f8f6125d8d92bd8e28d402a259b293
hattima-tim <irfanridwan20@gmail.com>
2025-05-11 21:40:08 +0600

    add og image

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,20 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "The Lost Billions",
   description: "Making sense of আওয়ামী লীগের লুটপাট",
+  openGraph: {
+    type: "website",
+    url: "https://the-lost-billions.vercel.app/",
+    title: "The Lost Billions",
+    description: "Making sense of আওয়ামী লীগের লুটপাট",
+    images: [
+      {
+        url: "https://the-lost-billions.vercel.app/og.png",
+        width: 1200,
+        height: 630,
+        alt: "The Lost Billions",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
